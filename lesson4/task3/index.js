0a9e0b884126342806abab84a2784cacd63bfbf3
Andrii Musykevych <87434917+AndriiMus@users.noreply.github.com>
2021-08-17 11:37:38 +0300

    прхождения первых уроков , установления ESLint

---
+++ lesson4/task3/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-plusplus */
+for(let i=1; i<=5;i++){
+ // eslint-disable-next-line no-plusplus
+ for(let j=1; j<=10;j++){
+console.log(i + ' x ' + j + ' = ' + i * j);
+ }
+console.log('\n=> ' + i + ' <=\n\n')
+}
