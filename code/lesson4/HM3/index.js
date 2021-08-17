7ada255fd2ea3de0b89fd117def74352ce1971ad
Andrii Musykevych <87434917+AndriiMus@users.noreply.github.com>
2021-08-17 13:51:37 +0300

    make 4 HM

--- code/lesson4/HM3/index.js
+++ code/lesson4/HM3/index.js
@@ -2,7 +2,7 @@
 const start = 1;
 const end = 50;
 let result = 0;
-for (let i = start; i <= end; i++;) {
+for (let i = start; i <= end; i++) {
   if (i % 5 === 0) {
     console.log(i);
   } else if (i % 2 === 0 && i % 4 !== 0) {
