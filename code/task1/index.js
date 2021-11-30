5054ecc60236460140bddf80b779a9ec05265670
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-11-30 18:42:04 +0200

    as

--- code/task1/index.js
+++ code/task1/index.js
@@ -1,10 +1,11 @@
 export function getTitleElement() {
   const titleElem = document.querySelector('title');
-  console.log(titleElem);
+  console.dir(titleElem);
+  return titleElem;
 }
 
 export function getInputElement(text) {
   const inputElem = document.querySelector(text);
-  console.log(inputElem);
+  console.dir(inputElem);
+  return inputElem;
 }
-
