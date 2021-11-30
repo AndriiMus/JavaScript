4992a82e69ba79e99bf1643fb3afbc4723b5a1f8
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-11-30 18:27:38 +0200

    sad

---
+++ code/task1/index.js
@@ -0,0 +1,10 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector('title');
+  console.log(titleElem);
+}
+
+export function getInputElement(text) {
+  const inputElem = document.querySelector(text);
+  console.log(inputElem);
+}
+
