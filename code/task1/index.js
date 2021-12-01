34a5e9ab0a8302a56e7111eec79ebc6f8afacb57
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-12-01 15:08:17 +0200

    task3
    task3

--- code/task1/index.js
+++ code/task1/index.js
@@ -1,11 +1,21 @@
-export function getItemsList() {
-  const elList = document.querySelectorAll('.technology');
-  console.dir(elList);
-  return elList;
+export function getTitle() {
+  const titleElem = document.querySelector('.title');
+
+  return titleElem.textContent;
 }
 
-export function getItemsArray() {
-  const elList = [...document.querySelectorAll('.tool')];
-  console.dir(elList);
-  return elList;
+export function getDescription() {
+  const aboutElem = document.querySelector('.about');
+
+  return aboutElem.innerText;
+}
+export function getPlans() {
+  const plansElem = document.querySelector('.plans');
+
+  return plansElem.innerHTML;
+}
+export function getGoal() {
+  const goalElem = document.querySelector('.goal');
+
+  return goalElem.outerHTML;
 }
