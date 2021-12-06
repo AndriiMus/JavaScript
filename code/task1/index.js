6add1d2c8b78b3ea60ad4c62a8ba0ac1e269e6d0
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-12-06 16:11:50 +0200

    мм
    мм

--- code/task1/index.js
+++ code/task1/index.js
@@ -1,7 +1,24 @@
-export function createButton(text) {
-  const body = document.querySelector('body');
-  const newButton = document.createElement('button');
-  newButton.textContent = text;
-  body.append(newButton);
-}
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+const logTarget = (text, color) => {
+  const eventsListElem = document.querySelector('.events-list');
 
+  eventsListElem.innerHTML += `<span style ="color: ${color}; margin-left: 8px;">${text}<span>`;
+};
+
+const greenDiv = logTarget.bind(null, 'DIV', 'green');
+const greenP = logTarget.bind(null, 'P', 'green');
+const greenSpan = logTarget.bind(null, 'Span', 'green');
+
+const grayDiv = logTarget.bind(null, 'DIV', 'gray');
+const grayP = logTarget.bind(null, 'P', 'gray');
+const graySpan = logTarget.bind(null, 'Span', 'gray');
+
+divElem.addEventListener('click', greenDiv);
+pElem.addEventListener('click', greenP);
+spanElem.addEventListener('click', greenSpan);
+
+divElem.addEventListener('click', grayDiv, true);
+pElem.addEventListener('click', grayP, true);
+spanElem.addEventListener('click', graySpan, true);
