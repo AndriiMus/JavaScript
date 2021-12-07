1c2aa2f8517b1828d9dc5d9b518cc0ae2b1f417e
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-12-07 16:20:29 +0200

    start to make ckear but

--- code/task1/index.js
+++ code/task1/index.js
@@ -1,6 +1,7 @@
 const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
+const clearButton = document.querySelector('.clear-btn');
 const logTarget = (text, color) => {
   const eventsListElem = document.querySelector('.events-list');
 
@@ -11,9 +12,9 @@ const greenDiv = logTarget.bind(null, 'DIV', 'green');
 const greenP = logTarget.bind(null, 'P', 'green');
 const greenSpan = logTarget.bind(null, 'Span', 'green');
 
-const grayDiv = logTarget.bind(null, 'DIV', 'gray');
-const grayP = logTarget.bind(null, 'P', 'gray');
-const graySpan = logTarget.bind(null, 'Span', 'gray');
+const grayDiv = logTarget.bind(null, 'DIV', 'grey');
+const grayP = logTarget.bind(null, 'P', 'grey');
+const graySpan = logTarget.bind(null, 'Span', 'grey');
 
 divElem.addEventListener('click', greenDiv);
 pElem.addEventListener('click', greenP);
