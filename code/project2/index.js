017244bc191ab63fbd5de5d9c685d050051b09bc
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-01 18:33:42 +0200

    удаление переменной defaultи логики дефолтный аватар

--- code/project2/index.js
+++ code/project2/index.js
@@ -2,10 +2,6 @@ const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
-const defaultAvatar = 'https://avatars.githubusercontent.com/u/87434917?v=4';
-
-userAvatarElem.src = defaultAvatar;
-
 const showUserBtnElem = document.querySelector('.name-form__btn');
 
 const userNameInputElem = document.querySelector('.name-form__input');
