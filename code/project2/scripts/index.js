f042df218a9fc9d5fab8401e05feff4f2837a254
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-09 17:09:58 +0200

    upd2

--- code/project2/scripts/index.js
+++ code/project2/scripts/index.js
@@ -1,11 +1,10 @@
 import { fetchRepositories, fetchUserData } from './gateways.js';
 import { renderUserData } from './user.js';
-import { renderRepos } from './repos.js';
+import { cleanReposList, renderRepos } from './repos.js';
 import { hiddenSpinner, showSpinner } from './spiner.js';
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
-const listElem = document.querySelector('.repo-list');
 
 const defaultUser = {
   avatar_url: 'https://avatars3.githubusercontent.com/u10001',
@@ -16,7 +15,7 @@ renderUserData(defaultUser);
 
 const onSearchUser = () => {
   showSpinner();
-  listElem.innerHTML = '';
+  cleanReposList();
   const userName = userNameInputElem.value;
   fetchUserData(userName)
     .then(userData => {
