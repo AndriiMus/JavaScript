891982464c77f62ca4e07849f8f055ffd82bc880
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-09 17:47:28 +0200

    апдейт ноер 2

--- code/project2/scripts/gateways.js
+++ code/project2/scripts/gateways.js
@@ -1,5 +1,10 @@
 export const fetchUserData = userName => {
-  return fetch(`https://api.github.com/users/${userName}`).then(response => response.json());
+  return fetch(`https://api.github.com/users/${userName}`).then(response => {
+    if (response.status === 200) {
+      response.json();
+    }
+    throw new Error('Failed to load data');
+  });
 };
 
 export const fetchRepositories = url => fetch(url).then(response => response.json());
