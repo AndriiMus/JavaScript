dbfff48bd0af16a6c3e4e072d8b5d3b6688ee202
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-01-09 17:54:38 +0200

    апдейт ....

--- code/project2/scripts/gateways.js
+++ code/project2/scripts/gateways.js
@@ -1,10 +1,16 @@
 export const fetchUserData = userName => {
   return fetch(`https://api.github.com/users/${userName}`).then(response => {
-    if (response.status === 200) {
+    if (response.ok) {
       response.json();
     }
     throw new Error('Failed to load data');
   });
 };
 
-export const fetchRepositories = url => fetch(url).then(response => response.json());
+export const fetchRepositories = url =>
+  fetch(url).then(response => {
+    if (response.ok) {
+      response.json();
+    }
+    throw new Error('Failed to load data');
+  });
